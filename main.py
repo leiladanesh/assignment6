f7c7563be404edd3d6fdd1f2ff0074bf7311755a
leila danesh <71027475+leiladanesh@users.noreply.github.com>
2021-08-21 15:42:23 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 
-from enum import Flag
+from typing import TextIO
 from pyfiglet import Figlet
 import qrcode
-
-list_product=[]
-
+product=[]
+PORODUCTS=[]   
 
 f = Figlet(font='standard')
-print (f.renderText('top stor '))
+print (f.renderText('top store '))
+
+
+def show_list():
+    for i in range(len(PORODUCTS)):
+        print( PORODUCTS[i]['id'],'\t',PORODUCTS[i]['name'],'\t',PORODUCTS[i]['price'],'\t',PORODUCTS[i]['count'],'\t')  
 
 
 def show_nemu():
@@ -20,186 +24,143 @@ def show_nemu():
     print ( '7- show list')
     print ( '8- exit')
 
-def database():
-    d_f = open('databeas.txt','r')
-    data = d_f.read()
-    data = data.strip()
-    return data
-
 def lode():
-    file = open('databeas.txt', 'r')
-    data = file.read()
-    product_list = data.split('\n')
-
-    for i in range(len(product_list)):
-        info_product = product_list[i].split(',')
-        kala= {}
-        kala['id']= info_product [0]
-        kala['name']= info_product [1]
-        kala['price']= info_product [2]
-        kala['count']= info_product [3]
-        list_product.append(kala)
-       
+    print('loding....')
+    f = open('databeas.txt','r')
+    rows = f.read().split('\n') 
     
-def show_list():
-    for i in range(len(list_product)):
-        print(list_product[i])
+    for i in range(len(rows)):
+        info = rows[i].split(',')
+        PORODUCTS.append({'id':int(info[0]),'name':info[1],'price':float(info[2]),'count':int(info[3])})
+    f.close()    
+    print('program is ready to use')
+
 
 def add_product():
-    n= int(input('add product ='))
-    add_dict ={}
-    for i in range(n):
-        print('product',(i+1))
-        id = input('id product: ')
-        name =input('name product: ')
-        price =input('price product: ')
-        count = input('count product: ')
-        add_dict['id ']= id
-        add_dict['name']= name
-        add_dict['price']=price
-        add_dict['count ']= count
-        list_product.append(add_dict)
-        print(name, (i+1),'added ')
-        
+    id = int(input('pleas enter id :'))
+    name = input('pleas enter name :')
+    price = float(input('pleas enter price: '))
+    count = int(input('pleas enter count :'))
+    PORODUCTS.append({'id':id , 'name': name , 'price':price, 'count':count})
+    print('add product is successfully')
 
-def save():
-    d_f = open('databeas.txt','w')
-    for i in range(len(list_product)):
-        save_dict = list_product[i]
-        id = save_dict['id']
-        name=save_dict['name']
-        price= save_dict['price']
-        count= save_dict['count']
-        d_f.write('%s,%s,%s,%s\n'%(id,name,price,count))
+def show_edit_menu():
+    print( '1- name ')
+    print( '2- price ')
+    print( '3- count ')
+    print( '4- exit')
 
-    d_f.close()
-    print('changes saved :')   
+def edit_product():
+    id = int(input('pleas Enter product id:'))
+    for i in range(len(PORODUCTS)):
+        if PORODUCTS[i]['id'] == id:
+            while True:
+                show_edit_menu()
+                choice = int(input('pleas Enter edit product '))
+                if choice == 1:
+                    PORODUCTS[i]['name'] = iput('please Enter new name :')
+                elif choice == 2:
+                    PORODUCTS[i]['price'] = float(input('pleas Enter new price ;'))
+                elif choice == 3:
+                    PORODUCTS[i]['count'] = int(input('please Enter new count')) 
+                elif choice == 4:
+                    break
+                else:
+                    print('value eroor')      
 
-def edit():
-    print ('number of product :', len (list_product))
-    while True:
-        p = int( input(' product \'s number: '))
-        if 0 < p <= len(list_product):
+def delet_product():
+    id = int(input(' pleas Enter remove id : '))
+    for i in range(len(PORODUCTS)):
+
+        if PORODUCTS[i]['id'] == id:
+            PORODUCTS.pop(i)
+            print('product remov ')
+            break
+
+def search_product():
+    word = input('pleas enter name or id :')
+    for i in range(len(PORODUCTS)):
+        if PORODUCTS[i]['name'] == word or str(PORODUCTS[i]['id'])== word:
+            print(PORODUCTS[i])
             break
         else:
-            print('bbbbbb')
-            continue
-    k -= 1
-    id =  list_product[k]['id']
-    name = list_product[k]['name']
-    price = list_product[k]['price']
-    count = list_product[k]['count']
-    print('ID: %s - NAME: %s - PRICE: %s - COUNT: %s'%(id,name,price,count))
-    while True:
-        print('1.edit id - 2.edit name - 3.edit price - 4. edit count -5 .exit editor ')
-        x= int(input('select option: '))
-        if x == 1:
-            new_id = input('new id =')
-            list_product[k]['id'] = new_id
-        elif x == 2:
-            new_name = input('new name = ')
-            list_product[k]['name']= new_name  
-        elif x == 3:
-            new_price = input('new price =')
-            list_product[k]['price'] = new_price
-        elif x == 4:
-            new_count = input('new count ')
-            list_product[k]['count']= new_count
-        elif x == 5:
-            print('exit')
-            break
-        else:
-            print('pleas chose a corrent number:')
-            continue
-    print(' new list')
-    show_list()    
+            print('there is not find ')    
 
-def search():
-    s = input('pleas Enter name product :') 
-    Flag= 0
-
-    for i in range(len(list_product)):
-        if s.upper().strip () == list_product[i]['name'].strip().upper():
-            index = i
-            Flag = 1
-            break
-    if Flag == 1:
-        print(' find product') 
-        print(list_product[index])
-    else:
-        print('this product is not find:')
-
-def qr_code():
+def buy_product():
+    basket = []
 
     while True:
-        code = input('product \'s code:')  
-        Flag = 0 
-        for i in range(len(list_product)):
-            if list_product[i]['id'] == code:
-                Flag =1
-                index = i
-                break
-            if Flag == 0:
-                print('This product \'s id is not exist ')
-                print ('EXIT QR code ...')
-                break
-            else:
-                id= list_product[index]['id']
-                name= list_product[index]['name']
-                price= list_product[index]['price']
-                count= list_product[index]['count'] 
-                qr_code = id  +name +price +count
-                img = qrcode.make(qr_code) 
-                img.save(qrcode_address)
-                print('product\'s QR Code is Ready :')
-                break
-
-def stor():
-    while True:
-        show_nemu()
-        choice = int(input('pleas choose a number:'))
-
-        if choice == 1:
-            add_product()
-        elif choice == 2:
-            edit()
-        elif choice == 3:
-            delete_product()
-        elif choice == 4:
-            search()
-        elif choice == 5:
-            show_list()
-        elif choice == 6:
-            buy() 
-        elif choice == 7:
-            qr_code()
-        elif choice == 8:
-            save()
-            exit()
+        id = int(input('pleas Enter product id :'))
+        for i in range(len(PORODUCTS)):
+            if PORODUCTS[i]['id'] == id:
+                count = int(input('pleas enter count: '))
+                if PORODUCTS[i]['count'] >= count:
+                    basket.append({'name':PORODUCTS[i]['name'],
+                                  'price':PORODUCTS[i]['price'],
+                                  'cont':count})
+                    PORODUCTS[i]['count'] -= count
+                    print('product added basket successfuly ')  
+                else:
+                    print('not exist')
+                    print('we have ,',PORODUCTS[i]['count'],'from this product') 
+        choice = input ('do you want to continue ? (Y/N)')
+        if choice == 'N' or 'n':
             break
-        
-        else:
-            print('please choose a correct number ') 
-            continue
-
-#lode()
-stor()                          
-                    
-
-
-       
-
-
-
-
-        
-
-        
-
+    print(basket)
+#     total_price = 0
+#     for i in range(len(basket)):
+#        # total_price += basket[i]['price']* basket[i]['count']
+#    # print('total price is: ',total_price)
+#     print('thanks')
+# 
+def qrcodee():
+    id = input('Enter id of product: ')
+    f=0
+    for i in range(len(PRODUCTS)):
+        if PRODUCTS[i]['id'] == id:
+            img = qrcode.make(PRODUCTS[i])
+            img.save('qrcode.png')
+            f=1
+    if f == 0:
+        print('Input wrong id!')
 
 
+def save_exit():
+
+    f =open('databeas.txt','w')
+
+    for i in range(len(PORODUCTS)):
+        row = str(PORODUCTS[i]['id']) + ',' + PORODUCTS[i]['name']+','+ str(PORODUCTS[i]['price']) + ',' + str(PORODUCTS[i]['count'])
+        f.write(str(row))
+        if i< len(PORODUCTS)-1:
+            f.write('\n')
+
+    f.close() 
+    exit()
+    print('your change save well ')  
 
 
+lode()
 
-show_nemu() 
+while True:
+    show_nemu()
+
+    choice = int(input('pleas choose a number:'))
+    if choice == 1:
+        add_product()
+    if choice == 2:
+        edit_product()
+    if choice == 3:
+        delet_product()        
+    if choice == 4:
+        search_product()
+    if choice == 5:
+        buy_product()
+    if choice == 6:
+        pass
+    if choice == 7: 
+        show_list()
+    if choice == 8:
+        save_exit()    
+          
 
